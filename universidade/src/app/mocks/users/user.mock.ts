2760d71c96a9409bf07c6fb0a817aa61b89cf9dd
Grazielle127 <grazigrazielle454@gmail.com>
2022-10-14 11:15:20 -0300

    criacao da interface do usuario e mock

---
+++ universidade/src/app/mocks/users/user.mock.ts
@@ -0,0 +1,14 @@
+import { User } from './../../interfaces/user/user.interface';
+export const users: User[] = [
+  {id: 1, name: 'Sérgio Pato',age: 34,course: 'Direito', semester: '4'},
+  {id: 2, name: 'Josiane Frias',age: 19,course: 'ADS', semester: '2'},
+  {id: 3, name: 'Jorge Dalmastti',age: 22,course: 'Economia', semester: '3'},
+  {id: 3, name: 'Emílio Camelo',age: 20,course: 'Astronomia', semester: '6'},
+  {id: 4, name: 'Edson Marques',age: 23,course: 'Psicologia', semester: '2'},
+  {id: 5, name: 'Marisia Vertunni',age: 21,course: 'Odontologia', semester: '1'},
+  {id: 6, name: 'Thiago Abravanull',age: 48,course: 'Ciência de dados', semester: '2'},
+  {id: 7, name: 'Joyce Warren',age: 25,course: 'Teologia', semester: '3'},
+  {id: 7, name: 'Larissa Ferraz',age: 18,course: 'Letras', semester: '1'},
+  {id: 7, name: 'Boris Spederson',age: 24,course: 'Enfermagem', semester: '7'}
+
+];
